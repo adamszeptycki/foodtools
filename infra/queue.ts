ad491cd40eef70304bdb6f1c3d7a364498a1276e
Adam Szeptycki <adamszeptycki@gmail.com>
2026-01-13 19:37:17 +0100

    queue

--- infra/queue.ts
+++ infra/queue.ts
@@ -1,6 +1,8 @@
 import { dbUrl, openAiApiKey } from "./config";
 import { documentsBucket } from "./storage";
 
+const MAX_RESERVED_CONCURENCY_FOR_SQS_TASK = 2;
+
 export const documentProcessingQueueDlq = new sst.aws.Queue("DocumentProcessingQueueDLQ");
 export const documentProcessingQueue = new sst.aws.Queue("DocumentProcessingQueue", {
 	visibilityTimeout: "5 minutes",
@@ -11,4 +13,7 @@ documentProcessingQueue.subscribe({
 	handler: "packages/core/src/workers/document-processor.handler",
 	timeout: "5 minutes",
 	link: [documentsBucket, dbUrl, openAiApiKey],
+	concurrency: {
+		reserved: MAX_RESERVED_CONCURENCY_FOR_SQS_TASK,
+	},
 });
